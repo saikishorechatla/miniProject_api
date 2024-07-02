43b14b2437cb602253811a27802669ca4a521c15
kishore <2100032302@kluniversity.in>
2024-07-03 00:15:37 +0530

    done 1

--- controllers/func.js
+++ controllers/func.js
@@ -1,5 +1,4 @@
 
-
 const Product = require('../models/product')
 
 async function getProducts(req, res){
--- router/route.js
+++ router/route.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-const Product = require('../models/product')
 const {getById ,getProducts,addProduct,updateById,deleteById} = require('../controllers/func')
 
 router.get('/',(req,res)=>{
